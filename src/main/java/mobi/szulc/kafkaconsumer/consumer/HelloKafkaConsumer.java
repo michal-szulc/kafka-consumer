49878556eac24087fbb233e182b9415256cebb31
michal-szulc <sap@szulc.mobi>
2020-10-01 08:33:10 +0200

    Initial commit

---
+++ src/main/java/mobi/szulc/kafkaconsumer/consumer/HelloKafkaConsumer.java
@@ -0,0 +1,12 @@
+package mobi.szulc.kafkaconsumer.consumer;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class HelloKafkaConsumer {
+    @KafkaListener(topics = "t_hello")
+    public void consume(String message){
+        System.out.println(message);
+    }
+}
